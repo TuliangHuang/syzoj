60a3a17948b83a3318caf806dcb9e562fca1352e
TuliangHuang <tulianghuang@gmail.com>
2025-09-08 08:44:19 +0000

    add question uid

--- models/question.ts
+++ models/question.ts
@@ -8,6 +8,10 @@ export default class Question extends Model {
   @TypeORM.PrimaryGeneratedColumn()
   id: number;
 
+  @TypeORM.Index({ unique: true })
+  @TypeORM.Column({ nullable: true, type: "varchar", length: 80 })
+  uid: string;
+
   @TypeORM.Column({ nullable: true, type: "varchar", length: 80 })
   title: string;
 
