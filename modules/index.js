7e1dd45038f09c426424f9e687bebe649fd9d33f
TuliangHuang <tulianghuang@gmail.com>
2025-11-26 11:43:45 +0000

    Revert "change home page url"
    This reverts commit d0dc5c047ee692987bc02634abe4cba696116f3d.

--- modules/index.js
+++ modules/index.js
@@ -8,11 +8,7 @@ let zh = require('../libs/timeago');
 TimeAgo.locale(zh);
 const timeAgo = new TimeAgo('zh-CN');
 
-app.get('/', (req, res) => {
-  res.redirect('/home');
-});
-
-app.get('/home', async (req, res) => {
+app.get('/', async (req, res) => {
   try {
     let ranklist = await User.queryRange([1, syzoj.config.page.ranklist_index], { is_show: true }, {
       [syzoj.config.sorting.ranklist.field]: syzoj.config.sorting.ranklist.order
